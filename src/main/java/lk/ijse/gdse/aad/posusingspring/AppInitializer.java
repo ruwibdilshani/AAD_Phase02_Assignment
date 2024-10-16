ac4ec002aa95f857724c05711eb8041dece40d0d
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-10-16 20:14:33 +0530

    Complete Appinitializer

---
+++ src/main/java/lk/ijse/gdse/aad/posusingspring/AppInitializer.java
@@ -0,0 +1,26 @@
+package lk.ijse.gdse.aad.posusingspring;
+
+import lk.ijse.gdse.aad.posusingspring.config.WebAppConfig;
+import lk.ijse.gdse.aad.posusingspring.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+
+
+}
