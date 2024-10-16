697dcf7ab2c0bea2efe37239a80f0b0124dd9caf
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-10-16 20:32:52 +0530

    Created SuperEntity

---
+++ src/main/java/lk/ijse/gdse/aad/posusingspring/entity/SuperEntity.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse.aad.posusingspring.entity;
+
+import java.io.Serializable;
+
+public interface SuperEntity extends Serializable {
+}
+// wby use this interface
+// 1. to make sure that all the entities are serializable
+//whats serializable
+// 1. Serializable is a marker interface in Java, which is used to "mark" Java classes so that objects of these classes may get certain capability.
